012bb9a8f7a15cd7992dbe10c04c57cc44fb6400
Tianyou Gu <tianyou.gu@gmail.com>
2020-08-14 10:55:23 -0700

    flask migrate fix

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os 
 from dotenv import load_dotenv
-from flask_migrate import Migrate
 
 from demo.app import create_app, db
 from demo.models import User
@@ -12,8 +11,6 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
